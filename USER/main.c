52eac8d9bb1a400659c4569e2a7134f1f9492f04
baby7305 <songhaibei7305@163.com>
2022-04-28 15:43:38 +0800

    ST-link调试程序

--- USER/main.c
+++ USER/main.c
@@ -1,10 +1,10 @@
-#include "stm32f10x.h"
+#include "sys.h"
+#include "delay.h"
+#include "usart.h"
 
- 
 /************************************************
- ALIENTEK 精英STM32F103开发板实验0
- 工程模板
- 注意，这是手册中的新建工程章节使用的main文件 
+ 实验0：ALIENTEK STM32F103开发板工程模板
+ 注意，这是手册中的调试章节使用的main文件
  技术支持：www.openedv.com
  淘宝店铺：http://eboard.taobao.com 
  关注微信公众平台微信号："正点原子"，免费获取STM32资料。
@@ -12,33 +12,17 @@
  作者：正点原子 @ALIENTEK
 ************************************************/
 
-
- void Delay(u32 count)
- {
-   u32 i=0;
-   for(;i<count;i++);
- }
- int main(void)
- {	
-  GPIO_InitTypeDef  GPIO_InitStructure;
-	 
-  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB|
-  RCC_APB2Periph_GPIOE, ENABLE);	    //使能PB,PE端口时钟
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5;			    //LED0-->PB.5 端口配置
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP; 	 //推挽输出
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;	 //IO口速度为50MHz
-  GPIO_Init(GPIOB, &GPIO_InitStructure);			     //初始化GPIOB.5
-  GPIO_SetBits(GPIOB,GPIO_Pin_5);					//PB.5 输出高
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5;	            //LED1-->PE.5推挽输出
-  GPIO_Init(GPIOE, &GPIO_InitStructure);	  	       //初始化GPIO
-  GPIO_SetBits(GPIOE,GPIO_Pin_5); 			 //PE.5 输出高 	  
+int main(void)
+ {		
+ 	u8 t=0;
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);//设置中断优先级分组为组2：2位抢占优先级，2位响应优先级
+	delay_init();	    	 //延时函数初始化	  
+	uart_init(115200);	 //串口初始化为115200
   while(1)
 	{
-	  GPIO_ResetBits(GPIOB,GPIO_Pin_5);
-	  GPIO_SetBits(GPIOE,GPIO_Pin_5);
-		Delay(3000000);
-		GPIO_SetBits(GPIOB,GPIO_Pin_5);
-		GPIO_ResetBits(GPIOE,GPIO_Pin_5);
-		Delay(3000000);
-	}
- }
+		printf("t:%d\r\n",t);
+		delay_ms(500);
+		t++;
+	}	 
+} 
+
