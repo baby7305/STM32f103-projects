13d0627784d05351c5ba6d02a65a2d96bc6930f3
baby7305 <songhaibei7305@163.com>
2022-04-29 09:38:36 +0800

    TIM4更换为TIM3

--- modbus/port/porttimer.c
+++ modbus/port/porttimer.c
@@ -42,12 +42,12 @@ xMBPortTimersInit( USHORT usTim1Timerout50us )
   uint16_t PrescalerValue = 0;
   
   //使能定时器4时钟
-  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE);
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
   
   //定时器时间基配置说明
   //HCLK为72MHz，APB1经过2分频为36MHz
-  //TIM4的时钟倍频后为72MHz（硬件自动倍频,达到最大）
-  //TIM4的分频系数为3599，时间基频率为72 / (1 + Prescaler) = 20KHz,基准为50us
+  //TIM3的时钟倍频后为72MHz（硬件自动倍频,达到最大）
+  //TIM3的分频系数为3599，时间基频率为72 / (1 + Prescaler) = 20KHz,基准为50us
   //TIM最大计数值为usTim1Timerout50u
   PrescalerValue = (uint16_t) (SystemCoreClock / 20000) - 1; 
   //定时器1初始化
@@ -55,25 +55,25 @@ xMBPortTimersInit( USHORT usTim1Timerout50us )
   TIM_TimeBaseStructure.TIM_Prescaler = PrescalerValue;
   TIM_TimeBaseStructure.TIM_ClockDivision = 0;
   TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
-  TIM_TimeBaseInit(TIM4, &TIM_TimeBaseStructure);
+  TIM_TimeBaseInit(TIM3, &TIM_TimeBaseStructure);
   //预装载使能
-  TIM_ARRPreloadConfig(TIM4, ENABLE);
+  TIM_ARRPreloadConfig(TIM3, ENABLE);
   
   //
 //  NVIC_PriorityGroupConfig(NVIC_PriorityGroup_1);
   //定时器4中断优先级
-  NVIC_InitStructure.NVIC_IRQChannel = TIM4_IRQn;
+  NVIC_InitStructure.NVIC_IRQChannel = TIM3_IRQn;
   NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
   NVIC_InitStructure.NVIC_IRQChannelSubPriority = 2;
   NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
   NVIC_Init(&NVIC_InitStructure);
   
   //清除溢出中断标志位
-  TIM_ClearITPendingBit(TIM4,TIM_IT_Update);
+  TIM_ClearITPendingBit(TIM3,TIM_IT_Update);
   //定时器4溢出中断关闭
-  TIM_ITConfig(TIM4, TIM_IT_Update, DISABLE);
+  TIM_ITConfig(TIM3, TIM_IT_Update, DISABLE);
   //定时器4禁能
-  TIM_Cmd(TIM4,  DISABLE);
+  TIM_Cmd(TIM3,  DISABLE);
   return TRUE;
 }
 
@@ -82,23 +82,23 @@ void
 vMBPortTimersEnable(  )
 {
     /* Enable the timer with the timeout passed to xMBPortTimersInit( ) */
-  TIM_ClearITPendingBit(TIM4, TIM_IT_Update);
-  TIM_ITConfig(TIM4, TIM_IT_Update, ENABLE);
+  TIM_ClearITPendingBit(TIM3, TIM_IT_Update);
+  TIM_ITConfig(TIM3, TIM_IT_Update, ENABLE);
   //设定定时器4的初始值
-  TIM_SetCounter(TIM4,0x0000); 
+  TIM_SetCounter(TIM3,0x0000); 
   //定时器4启动
-  TIM_Cmd(TIM4, ENABLE);
+  TIM_Cmd(TIM3, ENABLE);
 }
 
 void
 vMBPortTimersDisable(  )
 {
     /* Disable any pending timers. */
-  TIM_ClearITPendingBit(TIM4, TIM_IT_Update);
-  TIM_ITConfig(TIM4, TIM_IT_Update, DISABLE);
-  TIM_SetCounter(TIM4,0x0000); 
+  TIM_ClearITPendingBit(TIM3, TIM_IT_Update);
+  TIM_ITConfig(TIM3, TIM_IT_Update, DISABLE);
+  TIM_SetCounter(TIM3,0x0000); 
   //关闭定时器4
-  TIM_Cmd(TIM4, DISABLE);
+  TIM_Cmd(TIM3, DISABLE);
 }
 
 /* Create an ISR which is called whenever the timer has expired. This function
@@ -115,12 +115,12 @@ static void prvvTIMERExpiredISR( void )
   * @param  None
   * @retval None
   */
-void TIM4_IRQHandler(void)
+void TIM3_IRQHandler(void)
 {
-  if (TIM_GetITStatus(TIM4, TIM_IT_Update) != RESET)
+  if (TIM_GetITStatus(TIM3, TIM_IT_Update) != RESET)
   {
     //清除定时器T4溢出中断标志位
-    TIM_ClearITPendingBit(TIM4, TIM_IT_Update);
+    TIM_ClearITPendingBit(TIM3, TIM_IT_Update);
 
     prvvTIMERExpiredISR( );
   }
